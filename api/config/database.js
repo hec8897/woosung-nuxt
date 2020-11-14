69291ea7a05da1f2901c02e92f479a86ffa7250d
hec8897 <hec8897@naver.com>
2020-11-14 18:37:17 +0900

    db 셋팅

--- api/config/database.js
+++ api/config/database.js
@@ -11,6 +11,25 @@ const mysql = require("mysql");
    //process.env.DB_DATABASE
  })
 
+ function handleDisconnect() {
+    db.connect(function(err) {            
+      if(err) {                            
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000); 
+      }                                   
+    });                                 
+                                           
+    db.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+        return handleDisconnect();                      
+      } else {                                    
+        throw err;                              
+      }
+    });
+  }
+  
+  handleDisconnect();
  
 
 module.exports = client;
